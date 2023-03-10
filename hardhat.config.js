48ad2ac76b5d226502a3c8d7b96c9536c4dd3fa4
TpSmpc <wyhx04@gmail.com>
2023-03-10 13:01:09 +0800

    add deploy js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "smrevm1070",
+  networks: {
+    smrevm1070: {
+      url: "https://json-rpc.evm.testnet.shimmer.network/",
+      accounts: [process.env.RMS_EVM_PRIVATEKEY],
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.MUMBAI_PRIVATEKEY]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,23 +5,26 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+require("dotenv").config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const signers = ["0xEAFeeDe1634C730767f8BB52B228409A97e20834", "0x520da6bE41DdD56719b96685aa8a16f97c6907cA", "0xbC6FBA88AD1F470494095C793cEcB5AcF956f09a", "0x458a8E1cc5da9a205AFa66C3A6Fba40abf974203", "0x273a0D884aA94EB1cD8735D7B1F1451fC70a1131", "0xC4607f0F8337Ac925D4353ECf8e57f8057f6ce90"];
+  const requireNum = 4;
+  const MultiSignWallet = await hre.ethers.getContractFactory("MultiSignWallet");
+  const msw = await MultiSignWallet.deploy(signers, requireNum);
+  await msw.deployed();
+  console.log(`MultiSignWallet deployed to ${msw.address}`);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const wToken = "0xCB03Ebd74417AA792fC68a750F35c7b8F585027B";
+  const MultiSignERC20Wallet = await hre.ethers.getContractFactory("MultiSignERC20Wallet")
+  const msew = await MultiSignERC20Wallet.deploy(signers, requireNum, wToken);
+  await msew.deployed();
+  console.log(`MultiSignERC20Wallet deployed to ${msew.address}`);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const WrapERC20 = await hre.ethers.getContractFactory("WrapERC20")
+  const werc20 = await WrapERC20.deploy("SETH", 18);
+  await werc20.deployed();
+  console.log(`WrapERC20 deployed to ${werc20.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
